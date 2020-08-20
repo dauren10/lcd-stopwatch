7e89b6aaffa6d06afd3073a7df989e6b2c9c4e94
Dauren Shalabayev <dauren10@yandex.kz>
2020-08-20 17:15:40 +0600

    1

--- timer.ino
+++ timer.ino
@@ -4,12 +4,16 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 unsigned long t;
+
+unsigned long tr;
 int ms;
 int sec;
 String mysec;
 String mymin;
 int min;
 int hour;
+boolean buttonWasUp = true;
+boolean ledEnabled = false;
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
@@ -19,16 +23,56 @@ void setup()
   lcd.init();
   // Print a message to the LCD.
   lcd.backlight();
+  pinMode(8, OUTPUT);
+  pinMode(2, INPUT);
  
 }
 
+int
+
+
+
+
+
+timer(boolean button,boolean button2){
+  Serial.println(button);
+  Serial.println(button2);
+  return millis();
+}
 
 void loop()
 {
+ 
+     
+   // узнаем, отпущена ли кнопка сейчас
+   boolean buttonIsUp = digitalRead(2);
+ 
+   // если кнопка была отпущена и не отпущена сейчас
+   if (buttonWasUp && !buttonIsUp) {
+    
+      // исключаем дребезг контактов тактовой кнопки
+      delay(10);
 
+    // и считываем сигнал с кнопки снова
+    buttonIsUp = digitalRead(2);
+
+      // если кнопка нажата, то переворачиваем сигнал светодиода
+      if (!buttonIsUp) {
+         ledEnabled = !ledEnabled;
+         digitalWrite(8, ledEnabled);
+         tr = millis();
+      }
+      
+   }
   
-    t= millis();
-    ms = t%1000;
+  
+ 
+   // запоминаем состояние кнопки для новой итерации
+   buttonWasUp = buttonIsUp;
+
+   
+    t=millis()-tr;
+     ms = t%1000;
     t = t/1000;
     sec = t%60;
     if(sec<10){
@@ -59,8 +103,8 @@ void loop()
     
      lcd.print(mysec);
     
-    
-    delay(1000);
+
+
 
 
 }
